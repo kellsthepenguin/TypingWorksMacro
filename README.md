926364eacc7d2c4e3d386d78e564dd282d5befda
Kim Jioo <jwkim101201@gmail.com>
2020-11-03 08:17:03 +0900

    some commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# TypingWorksMacro
+
+some https://typing.works macro
+
+## Setup
+
+### Download ChromeDriver
+
+first, go to https://chromedriver.chromium.org/downloads,
+find your browser version, then click it. and click your os,
+and click it.
+
+### Place ChromeDriver
+place chromedriver to ./chromedriver/
+
+### Enjoy
+enjoy!
---
+++ main.py
@@ -0,0 +1,22 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome("./chromedriver/chromedriver")
+driver.implicitly_wait(3)
+
+driver.get("https://typing.works/")
+
+i = 0
+
+while i < 5:
+    time.sleep(1)
+    text = driver.find_element_by_id('sing').get_attribute("innerHTML")
+    ipt = driver.find_element_by_id('ipt')
+
+    ipt.send_keys(text)
+    time.sleep(0.1)
+    ipt.send_keys(Keys.RETURN)
+    i += 1
+    time.sleep(4)
